c76bfe435c707cef42b5b2ebdeba52c877aaedec
Pranav1176 <m.pranav1176@gmail.com>
2026-02-26 15:28:52 +0530

    [Pranav] added WelcomePage

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,5 +1,6 @@
+// version 1.0
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to Palindrome Checker App Management System");
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker App Management System \nversion 1.0 \nSystem intiliazed succesfully");
     }
 }
