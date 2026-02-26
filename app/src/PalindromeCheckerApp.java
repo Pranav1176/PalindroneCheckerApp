943c56ef77f3701d1a590ed80d4db9c39226747e
Pranav1176 <m.pranav1176@gmail.com>
2026-02-26 15:53:02 +0530

    [Pranav Added PalindromeResult

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,6 +1,20 @@
 // version 1.0
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App Management System \nversion 1.0 \nSystem intiliazed succesfully");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equals(reverse)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
     }
 }
